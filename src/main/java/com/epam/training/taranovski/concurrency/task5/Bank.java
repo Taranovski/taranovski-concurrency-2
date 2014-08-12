3bb64932a0bede4fb715c29fcb1e8053f4eae414
user <user@U3>
2014-08-12 09:03:09 +0300

    bank bank

--- src/main/java/com/epam/training/taranovski/concurrency/task5/Bank.java
+++ src/main/java/com/epam/training/taranovski/concurrency/task5/Bank.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.epam.training.taranovski.concurrency.task5;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -13,5 +13,44 @@ import java.util.List;
  * @author Alyx
  */
 public class Bank {
+
     List<Account> accounts;
+
+    public void makeTransaction(int accountNumber1, int accountNumber2, BigDecimal amount) {
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean checkAllPositive() {
+        boolean allPositive = true;
+        for (Account account : accounts) {
+            allPositive = allPositive & (account.getSumm().signum() >= 0);
+        }
+        return allPositive;
+    }
+
+    /**
+     *
+     * @param accountNumber
+     * @param amount
+     * @return
+     */
+    private boolean checkAmount(int accountNumber, BigDecimal amount) {
+        return accounts.get(accountNumber).getSumm().compareTo(amount) >= 0;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public BigDecimal getAllSumm() {
+        BigDecimal summ = new BigDecimal(0);
+        for (Account account : accounts) {
+            summ = summ.add(account.getSumm());
+        }
+        return summ;
+    }
 }
